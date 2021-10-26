d0e3c9e46511d4c4265970bfe33f3130ed82ad4c
angeliquebesi <besineaud85@gmail.com>
2021-10-26 17:31:00 +0200

    paramétrage extension eslint et mise a jour nom de fichier

--- src/Container/Pages/SpellPotion.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function SpellPotion() {
-    return (
-        <div>
-
-        </div>
-    )
-}
--- src/Container/Pages/SpellPotion.jsx
+++ src/Container/Pages/SpellPotion.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function CarDeck() {
+export default function SpellPotion() {
   return <div></div>;
 }
