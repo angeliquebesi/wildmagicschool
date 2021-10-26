46783cfcade18eb68e89edd74c3c7303e26f964b
angeliquebesi <besineaud85@gmail.com>
2021-10-26 16:43:53 +0200

    contenu de la page home

--- src/Container/Pages/Home.js
+++ src/Container/Pages/Home.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import "./Home.css";
+import NavBar from '../../Components/NavBar.js';
+import Footer from '../../Components/Footer.js';
 
 export default function Home() {
     return (
-        <div>
+        <div className = "container">
+            <NavBar />
+            <p className ="heropresentation text-center"> Welcome on the website of The Wild Magic School, we propose you an epic aventure which you will not regret ! Just press the "start" button and the hat will chose your house.When your house is finally chosen, you will have to make multiple choices to stay in the aventure. </p>
+            <button className = "buttonstart" href ="" >Start game</button>
+            <Footer />
             
         </div>
     )
