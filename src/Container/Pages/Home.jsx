87f2a1b21de93f58e8b16f8c26ab96f69d4c3d08
angeliquebesi <besineaud85@gmail.com>
2021-10-27 15:44:28 +0200

    configuration terminée

--- src/Container/Pages/Home.jsx
+++ src/Container/Pages/Home.jsx
@@ -9,17 +9,6 @@ export default function Home() {
   return (
     <div className="container">
       <NavBar />
-      <div className="texthero">
-        <p className="heropresentation text-center">
-          Welcome on the website of The Wild Magic School, we propose you an
-          epic aventure which you will not regret ! Just press the start button
-          and the hat will chose your house.When your house is finally chosen,
-          you will have to make multiple choices to stay in the aventure.
-        </p>
-        <button type="button" className="buttonstart" href="">
-          Start game
-        </button>
-      </div>
       <Footer />
     </div>
   );
