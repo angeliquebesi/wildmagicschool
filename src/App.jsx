5a8b17382fd73cf914cce827f8b4478a06cd680a
angeliquebesi <besineaud85@gmail.com>
2021-10-27 09:47:06 +0200

    fin de configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
 import React from "react";
-import Home from "./Container/Pages/Home.js";
-import Houses from "./Container/Pages/Houses.js";
-import SpellPotion from "./Container/Pages/SpellPotion.js";
+import Home from "./Container/Pages/Home";
+import Houses from "./Container/Pages/Houses";
+import SpellPotion from "./Container/Pages/SpellPotion";
 
 function App() {
   return (
