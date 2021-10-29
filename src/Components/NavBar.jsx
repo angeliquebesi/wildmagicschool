88352758fe1423976a6bb843f3c403d39b06df68
Victor Pagnier <victor.pagnier@gmail.com>
2021-10-29 09:49:15 +0200

    NavBar modified + background

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import Logo from "../logo-final.png";
+import "./NavBar.css";
 
 export default function NavBar() {
-  return <div>f</div>;
+  return (
+    <header>
+      <img src={Logo} alt="logo" className="logo" />
+      <ul className="list">
+        <li>START THE GAME</li>
+        <li>THE HOUSES</li>
+        <li>CHARACTERS</li>
+        <li>CONTACT</li>
+      </ul>
+    </header>
+  );
 }
