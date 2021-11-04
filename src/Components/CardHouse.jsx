7998cddf2e6643fa42ee76308d28839a07123c0d
Yohan Gerbaud <86349498+Ygerbaud@users.noreply.github.com>
2021-11-04 10:30:52 +0100

    Fin houses

--- src/Components/CardHouse.jsx
+++ src/Components/CardHouse.jsx
@@ -3,35 +3,36 @@ import "./CardHouse.css";
 import defautAvatar from "./Image/defautAvatar.png";
 
 const item = "";
-const CardHouse = ({ pers, id }) => {
-  console.log(pers);
-  return (
-    <div className="col-4 px-4">
-      <div id={`harry-${id}`}>
-        <div className="card  card-stretch text-center mx-auto">
-          <img
-            src={pers.image || defautAvatar}
-            alt={pers.name}
-            className="card-img-top img-fluid "
-          />
-          <div className="card-body">
-            <h2 className="card-title ">{pers.name}</h2>
-            <div className="card-text ">
-              <strong>Patronus: </strong>
-              {pers.patronus}
-              <br />
-              <strong>birth: </strong>
-              {pers.dateOfBirth}
-              <br />
-              <strong>Ancestry: </strong>
-              {pers.ancestry}
-            </div>
-            {item !== "" ? (<a className="btn btn-dark" target="_blank" href="">Select </a>) : null}
+const CardHouse = ({ pers, id }) => (
+  <div className="col-4 px-4">
+    <div id={`harry-${id}`}>
+      <div className="card  card-stretch text-center mx-auto">
+        <img
+          src={pers.image || defautAvatar}
+          alt={pers.name}
+          className="card-img-top img-fluid "
+        />
+        <div className="card-body">
+          <h2 className="card-title ">{pers.name}</h2>
+          <div className="card-text ">
+            <strong>Patronus: </strong>
+            {pers.patronus}
+            <br />
+            <strong>birth: </strong>
+            {pers.dateOfBirth}
+            <br />
+            <strong>Ancestry: </strong>
+            {pers.ancestry}
           </div>
+          {item !== "" ? (
+            <a className="btn btn-dark" target="_blank" href="">
+              Select
+            </a>
+          ) : null}
         </div>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default CardHouse;
