fc8f7d37f8d6733c1bd36eeb1f608dcd7841e9de
angeliquebesi <besineaud85@gmail.com>
2021-11-15 15:57:20 +0100

    test route

--- src/Components/ButtonReturnLesson/ButtonReturnLesson.jsx
+++ src/Components/ButtonReturnLesson/ButtonReturnLesson.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 import UserContext from "../../Context/UserContext";
 
 export default function ButtonReturnLesson({ type2 }) {
-  const { item } = useContext(UserContext);
+  const { userHouse } = useContext(UserContext);
 
   return (
     <div>
-      <Link to={`/hat/${item}/Marauder/${type2}`}>
+      <Link to={`/hat/${userHouse}/Marauder/${type2}`}>
         <button type="button" className="buttonstart">
           Back to choose your lesson
         </button>
