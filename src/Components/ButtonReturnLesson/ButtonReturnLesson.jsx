6dce36c603e82ed56e73ae2b2737eb573c1b7c12
angeliquebesi <besineaud85@gmail.com>
2021-11-16 16:01:51 +0100

    actualisation du button return to choose lesson

--- src/Components/ButtonReturnLesson/ButtonReturnLesson.jsx
+++ src/Components/ButtonReturnLesson/ButtonReturnLesson.jsx
@@ -1,13 +1,14 @@
 import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import UserContext from "../../Context/UserContext";
 
-export default function ButtonReturnLesson({ type2 }) {
+export default function ButtonReturnLesson() {
   const { userHouse } = useContext(UserContext);
+  const { type } = useParams();
 
   return (
     <div>
-      <Link to={`/hat/${userHouse}/Marauder/${type2}`}>
+      <Link to={`/hat/${userHouse}/Marauder/${type}`}>
         <button type="button" className="buttonstart">
           Back to choose your lesson
         </button>
