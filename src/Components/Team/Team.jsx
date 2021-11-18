570a56e14e2e60473f7f6f895182b0c2609e2b67
Victor Pagnier <victor.pagnier@gmail.com>
2021-11-18 20:18:31 +0100

    modified about us

--- src/Components/Team/Team.jsx
+++ src/Components/Team/Team.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Homme from "../../images/homme.png";
+import AfterHomme from "../../images/after-homme.png";
 import Femme from "../../images/femme.png";
+import AfterFemme from "../../images/after-femme.png";
 import "./Team.css";
 
-const Team = ({ name, alias, nickname }) => (
-  <div className="person-item">
-    <div>
+const Team = ({ name, alias, nickname }) => {
+  const [isHomme, setIsHomme] = useState(Homme);
+  const [isFemme, setIsFemme] = useState(Femme);
+  return (
+    <div className="person-item">
       <h3 className="name">{name}</h3>
       <h4 className="alias">{alias}</h4>
-      {name !== "Angélique" ? <input type="image" className="img-homme" src={Homme} alt="Homme" onClick={() => window.open(`https://github.com/${nickname}`)} /> : <input type="image" className="img-femme" src={Femme} alt="Homme" onClick={() => window.open(`https://github.com/${nickname}`)} />}
+      {name !== "Angélique" ? <input type="image" src={isHomme} onMouseEnter={() => setIsHomme(AfterHomme)} onMouseLeave={() => setIsHomme(Homme)} onClick={() => window.open(`https://github.com/${nickname}`)} alt="Homme" /> : <input type="image" src={isFemme} onMouseEnter={() => setIsFemme(AfterFemme)} onMouseLeave={() => setIsFemme(Femme)} onClick={() => window.open(`https://github.com/${nickname}`)} alt="Femme" />}
     </div>
-  </div>
-);
+  );
+};
 
 export default Team;
